aeb9def191a578e419231d55e551e49f9533742a
Code-with-Tuaha <tuahaathar6@gmail.com>
2025-07-15 08:57:57 -0700

    Landing page Complete

--- src/app/_component/ProductLayout.tsx
+++ src/app/_component/ProductLayout.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 interface detail {
     name: string,
-    project: string
+    project: string,
+    link: string
 }
 
-const ProductLayout = ({name, project}: detail) => {
+const ProductLayout = ({name, project, link}: detail) => {
     return (
         <div className='w-full flex flex-col gap-1 group'>
             <div className='w-full p-4 flex justify-between bg-white rounded-lg'>
@@ -20,7 +22,7 @@ const ProductLayout = ({name, project}: detail) => {
                     <span className='w-2 h-2 bg-black/50 group-hover:bg-green-400 max-md:bg-green-400  duration-300 rounded-full'></span>
                 </div>
             </div>
-            <div className='w-full bg-white rounded-xl p-2'>
+            <Link href={link} target='_blank' className='w-full bg-white rounded-xl p-2'>
                 <div className='w-full h-full relative overflow-hidden rounded-xl'>
                     <Image src={project} alt='Project-1' width={1500} height={1024} className='w-full h-[60vh] max-lg:h-[50vh] max-sm:h-[40vh] group-hover:scale-110 duration-500 rounded-xl object-cover object-top'></Image>
                     <div className='absolute top-[50%] left-[50%] -translate-[50%] z-10 group-hover:scale-75 duration-300'>
@@ -28,7 +30,7 @@ const ProductLayout = ({name, project}: detail) => {
                     </div>
                     <div className='absolute top-0 left-0 group-hover:backdrop-blur-md duration-300 w-full h-screen z-0'></div>
                 </div>
-            </div>
+            </Link>
         </div>
     )
 }
--- src/app/_component/Project.tsx
+++ src/app/_component/Project.tsx
@@ -8,12 +8,12 @@ const Project = () => {
                 <p className='text-lg text-black/50 font-semibold'>We&apos;ve helped businesses across industries achieve their goals. Here are some of our recent projects.</p>
             </div>
             <div className='w-full h-full grid grid-cols-2 gap-1 max-lg:grid-cols-1 pt-10'>
-                <ProductLayout name='Barc*' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-6.png?updatedAt=1752592398298' />
-                <ProductLayout name='Designer' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-4.png?updatedAt=1752592398359' />
-                <ProductLayout name='Xales' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-7.png?updatedAt=1752592397152' />
-                <ProductLayout name='Other Portfolio' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-8.png?updatedAt=1752592396814' />
-                <ProductLayout name='Gymlytic' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-2.png?updatedAt=1752592398311' />
-                <ProductLayout name='E-commerce' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-3.png?updatedAt=1752592397921' />
+                <ProductLayout name='Barc*' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-6.png?updatedAt=1752592398298' link='https://advanceclasses.framer.website/' />
+                <ProductLayout name='Designer' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-4.png?updatedAt=1752592398359' link='https://designer.prodigi-studios.com/' />
+                <ProductLayout name='Xales' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-7.png?updatedAt=1752592397152' link='https://xales.prodigi-studios.com/' />
+                <ProductLayout name='Other Portfolio' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-8.png?updatedAt=1752592396814' link='https://tuaha.vercel.app/' />
+                <ProductLayout name='Gymlytic' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-2.png?updatedAt=1752592398311' link='https://gymlytic.prodigi-studios.com/' />
+                <ProductLayout name='E-commerce' project='https://ik.imagekit.io/qss3mmw4a/Agency/website-3.png?updatedAt=1752592397921' link='https://saadshopifyweb.vercel.app/' />
             </div>
         </div>
     )
